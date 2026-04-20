f59f2682fc1323777e75cc08dfa5674c142a854e
rchrdkvcs <gumimacihun@gmail.com>
2026-04-20 23:06:19 +0200

    update UI colors and restructure app layout with new Header component

---
+++ app/components/Header.vue
@@ -0,0 +1,20 @@
+<script lang="ts"></script>
+
+<template>
+  <UHeader
+    :ui="{
+      root: 'bg-transparent border-none backdrop-blur-none!',
+    }"
+  >
+    <template #left>
+      <NuxtLink to="/">
+        <NuxtImg src="/images/etoile_michelin.png" class="size-8" />
+        <div class=""></div>
+      </NuxtLink>
+    </template>
+
+    <template #right> </template>
+  </UHeader>
+
+  <div class="absolute top-0 left-0 w-full bg-linear-to-b from-black/25 to-transparent h-32"></div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,35 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
+    "@nuxt/eslint",
+    "@nuxt/ui",
+    "@nuxt/image",
   ],
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   routeRules: {
-    '/': { prerender: true }
+    "/": { prerender: true },
   },
 
-  compatibilityDate: '2025-01-15',
+  compatibilityDate: "2025-01-15",
 
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
-})
+  ui: {
+    colorMode: false,
+    experimental: {
+      componentDetection: true,
+    },
+  },
+
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+
+  eslint: false,
+});
